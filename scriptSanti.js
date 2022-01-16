d8d9bdd4ce51adf4d11474972a8da0e7dd7f312d
Santi Zambrino <santizambrino@gmail.com>
2022-01-16 21:34:36 +0100

    movimiento

--- scriptSanti.js
+++ scriptSanti.js
@@ -2,20 +2,13 @@ const celdas = document.getElementsByTagName("td");
 const flechas = document.getElementsByClassName("flechas");
 const filas = document.getElementsByTagName("tr");
 const columnas = document.getElementsByTagName("td");
-const tabla = document.getElementsByTagName("table")[0];
-let encontrado = false;
-// var posicionBueno = [[0]];
 
 window.onload = () => {
   const jugar = document.getElementById("jugar");
   jugar.onclick = comienzoJuego;
 
-
-
-//   flechas[0].addEventListener("click", moverArriba);
   flechas[1].addEventListener("click", moverDerecha);
-//   flechas[2].addEventListener("click", moverAbajo);
-//   flechas[3].addEventListener("click", moverIzquierda);
+  flechas[3].addEventListener("click", moverIzquierda);
 };
 
 function comienzoJuego() {
@@ -38,61 +31,37 @@ function comienzoJuego() {
   for (let i = 0; i < flechas.length; i++) {
     flechas[i].style.visibility = "visible";
   }
+}
 
-  for (let i = 0; i < flechas.length; i++) {
-    flechas[i].addEventListener("click", console.log("hola"));
+function moverDerecha() {
+  let contador = 1
+  for (let i = 0; i < columnas.length; i++) {
+    if(i == ( contador * filas.length -1)){
+      console.log(i)
+      console.log(contador * filas.length -1)
+      contador++;
+    }
+    else if ((columnas[i].textContent == 'Robert') && (i != (columnas.length -1))) {
+      columnas[i].textContent = '-'
+      columnas[i+1].textContent = 'Robert';
+      i = columnas.length;
+    }
   }
 }
 
-function moverArriba() {}
 
-function moverDerecha() {
-    for (let i = 0; i < filas.length; i++) {
-        console.log(filas.length)
-        for (let j = 0; j < columnas.length; j++) {
-            console.log(columnas.length)
-          if (j != (filas.length-1) && columnas[j].textContent == "Robert") {
-            columnas[j + 1].textContent = "Robert";
-            columnas[j].textContent = "-";
-            j += filas.length;
-          }
-          
-          if (encontrado) j=columnas.length;
-        }
-        encontrado = false;
-      }
+function moverIzquierda() {
+  let contador = 1
+  for (let i = 0; i < columnas.length; i++) {
+    if(i % filas.length == 0){
+      console.log(i)
+      console.log(contador * filas.length -1)
+      contador++;
+    }
+    else if ((columnas[i].textContent == 'Robert') && (i != (columnas.length -1))) {
+      columnas[i].textContent = '-'
+      columnas[i-1].textContent = 'Robert';
+      i = columnas.length;
+    }
+  }
 }
-// function moverDerecha() {
-//   console.log(filas);
-//   console.log(columnas);
-
-//   // if(posicionBueno!=filas.length-1){
-
-//   //     posicionBueno = posicionBueno + 1;
-//   //     celdas[posicionBueno].textContent = 'Robert';
-
-//   //     var posicionBuenoAnt = posicionBueno - 1;
-//   //     celdas[posicionBuenoAnt].textContent = '-';
-//   // }
-
-//   // console.log(posicionBueno);
-// }
-
-function moverAbajo() {
-  // console.log('abajo')
-}
-
-// function moverIzquierda() {
-//   let posicion;
-
-//   for (let i = 0; i < filas.length; i++) {
-//     for (let j = 0; j < columnas.length; j++) {
-//       if (j != 0 && columnas[j].textContent == "Robert") {
-//         posicion = j - 1;
-//         columnas[posicion].textContent = "Robert";
-//         columnas[posicion + 1].textContent = "-";
-//       }
-//     }
-//   }
-//   console.log(posicion);
-// }
